1feba76002c73b4b2299d8f3137d83f04ad3a953
Nino Chimento <saninoc84@gmail.com>
2020-03-25 14:10:57 +0100

    grafica minimale index..creo bottone shoow

--- resources/views/Posts/index.blade.php
+++ resources/views/Posts/index.blade.php
@@ -16,7 +16,7 @@
                <div class="card-body">
                <h5 class="card-title">Genere : {{$post->genre}}</h5>
                <p class="card-text">{{$post->description}}</p>
-                 <a href="#" class="btn btn-primary">Dettagli</a>
+               <a href="{{route("Posts.show",$post)}}" class="btn btn-primary">Dettagli</a>
                 </div>
              </div>
         @endforeach
