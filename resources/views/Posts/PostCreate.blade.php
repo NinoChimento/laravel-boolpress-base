b8eaa88c447e8c16cb9e74849a1672da4af97b82
Nino Chimento <saninoc84@gmail.com>
2020-03-25 17:07:15 +0100

    piccolo ritocco

--- resources/views/Posts/PostCreate.blade.php
+++ resources/views/Posts/PostCreate.blade.php
@@ -8,6 +8,15 @@
     <title>Document</title>
 </head>
 <body>
+       @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+             @foreach ($errors->all() as $error)
+               <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+       </div>
+    @endif
 <form action="{{route("posts.store")}}" method="POST">
     @csrf
     @method("POST")
--- resources/views/Posts/PostEdit.blade.php
+++ resources/views/Posts/PostEdit.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.layout')
 @section('main')
 
+   @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+             @foreach ($errors->all() as $error)
+               <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+       </div>
+    @endif
 <form action="{{route("posts.update",$post)}}" method="POST">
     @csrf
     @method("PATCH")
