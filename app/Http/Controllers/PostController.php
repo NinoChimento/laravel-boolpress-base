07001d2533e107622847a72ac9090e2ae6bdd497
Nino Chimento <saninoc84@gmail.com>
2020-03-25 13:48:47 +0100

    creo index e stampo i dati con dd

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class PostController extends Controller
 {
     /**
@@ -13,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return view("Posts.index",compact("posts"));
     }
 
     /**
