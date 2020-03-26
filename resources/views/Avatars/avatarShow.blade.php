22d0e3ec2cbd5f4e773284b854aaf2337de314fc
Nino Chimento <saninoc84@gmail.com>
2020-03-26 15:41:44 +0100

    stampo dettagli utente in avatar show

--- resources/views/Avatars/avatarShow.blade.php
+++ resources/views/Avatars/avatarShow.blade.php
@@ -1,1 +1,10 @@
-@dd($avatar);
+@extends('layouts.layout')
+@section('main')
+
+    <div class="card">
+        <h1>name :{{ $avatar->user->name}}</h1>
+        <img src="{{$avatar->img}}" alt="">
+        <h4>{{$avatar->user->email}}</h4>
+        <h4>Create il : {{$avatar->created_at}}</h4>
+    </div>
+@endsection
