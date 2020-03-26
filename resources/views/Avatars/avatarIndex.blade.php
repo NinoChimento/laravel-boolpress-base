ad36baa8eb8451ce8e193e616100817353bfd8bf
Nino Chimento <saninoc84@gmail.com>
2020-03-26 15:28:59 +0100

    creo sezione avatar con nome utente

--- resources/views/Avatars/avatarIndex.blade.php
+++ resources/views/Avatars/avatarIndex.blade.php
@@ -1,1 +1,8 @@
-@dd($avatars);
+@extends('layouts.layout')
+@section('main')
+    @foreach ($avatars as $avatar)
+<img src="{{$avatar->img}}" alt="">
+<h2>Nome Utente : {{$avatar->user->name}}</h2>
+    @endforeach
+@endsection)
+{{-- @dd($avatars); --}}
